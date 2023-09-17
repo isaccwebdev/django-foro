9c51131bec0d702ec8a0ba7084eb27a35e74a270
isaccwebdev <dangdev97@gmail.com>
2023-09-17 21:44:43 +0200

    auth_user_added

--- core/core/settings.py
+++ core/core/settings.py
@@ -146,12 +146,11 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
 STATIC_URL = 'static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = "apps.users.User"
